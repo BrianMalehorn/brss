4a2dab23b4aab4b1338e0461c91ee0648b3eefc2
Brian Malehorn - andrew <malehorn@andrew.cmu.edu>
2013-03-30 22:56:13 -0400

    fixed ip address typo

--- main.ts
+++ main.ts
@@ -9,13 +9,15 @@ import express = module('express');
 var app = express();
 app.use(express.bodyParser());
 
-var ipAddress : string = process.env.OPENSHIFT_INTERAL_IP || "localhost";
-var port : number = process.env.OPENSHIFT_INTERAL_PORT || 8000;
+var ipAddress : string = process.env.OPENSHIFT_INTERNAL_IP || "localhost";
+var port : number = process.env.OPENSHIFT_INTERNAL_PORT || 8000;
 
 app.get("/", function(request, response) : void {
     response.send("Hello, world!");
 });
 
+console.log("%s: trying on %s:%d",
+            (new Date()).toString(), ipAddress, port);
 app.listen(port, ipAddress, -1, function() : void {
     console.log("%s: started on %s:%d",
                 (new Date()).toString(), ipAddress, port);
