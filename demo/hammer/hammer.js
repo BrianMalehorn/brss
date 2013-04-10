a2c591412bef35c6891f6840f2093def58ac2173
Brian Malehorn <bmalehorn@gmail.com>
2013-04-09 20:35:04 -0400

    rearranged demo folder, added README's

---
+++ demo/hammer/hammer.js
@@ -0,0 +1,1 @@
+../../static/hammer.js
--- demo/server.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.get("/:filename", function(request, response) {
-    response.sendfile(request.params.filename);
-});
-
-app.listen(8000);
