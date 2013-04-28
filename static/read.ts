e714318f57aaf16ed0a09a8de981c98f73bffeb8
Brian Malehorn - y560p <malehorn@andrew.cmu.edu>
2013-04-28 03:43:13 -0400

    offload div's out of view

--- static/read.ts
+++ static/read.ts
@@ -17,13 +17,15 @@ module Read {
 
   Misc.assert(NUM_INITIAL_ITEMS > NTH_LAST);
 
-  declare var fillDiv : (JQuery) => JQuery;
-  declare var emptyDiv : (JQuery) => JQuery;
+  declare var fillDiv : (JQuery) => void;
+  declare var emptyDiv : (JQuery) => void;
   declare var insertItems : (items : ClItem[]) => void;
+
+  var feedIdThatLoadsMoreItems : string = "";
 
   // given a div with div.data("self") being an item, empties it out and fills
   // its contents with the contents of item.
-  var fillDiv = function(div : JQuery) : JQuery {
+  var fillDiv = function(div : JQuery) : void {
     var item : ClItem = div.data("self");
     (div
      .empty()
@@ -36,55 +38,81 @@ module Read {
              .html(item.description)
              .addClass('itemDescription'))
      .addClass('itemContainer'));
-    return div;
   };
 
-  var emptyDiv = function(div : JQuery) : JQuery {
-    // fix the height of the div so when you empty it, it's size doesn't change
+  var emptyDiv = function(div : JQuery) : void {
+    // fix the height of the div so when you empty it, its size doesn't change
     div.height(div.height());
-    return div.empty();
+    div.empty();
   };
 
   // Put all these items into #readList, setting all the extra insertion
   // stuff as well.
   var insertItems = function(items : ClItem[]) : void {
 
+    var divs : JQuery[] = [];
+
     for (var i = 0; i < items.length; i++) {
       var item = items[i];
       var div = $("<div>").data("self", item);
       fillDiv(div);
+      divs.push(div);
       $("#readList").append(div);
     }
 
-    // when the last item is in view, load the next ADDITIONAL_ITEMS items and
-    // add them recursively.
-
+    // now that you've added some, select which feed should load more items
     var containers : JQuery = $(".itemContainer");
     if (containers.length == 0)
       return;
     var index : number = containers.length - 1 - NTH_LAST;
-    var last = $(containers[index]);
-    console.log(last);
-    last.bind('inview', function(event, isInView : bool) {
-      if (isInView) {
-        var item : ClItem = last.data("self");
-        last.unbind('inview');
-        $.ajax({
-          type: 'get',
-          url: "/next-n-items",
-          data: {
-            feedId: item.feedId,
-            date: item.date,
-            n: NUM_ADDITIONAL_ITEMS,
-          },
-          success: function(data : string) {
-            var items : ClItem[] = JSON.parse(data);
-            insertItems(items);
-          },
-        });
-      }
-    });
+    feedIdThatLoadsMoreItems = $(containers[index]).data("self")._id;
 
+    // for every div, make it hide/show itself when scrolled
+    // over. Additionally, if it is The Feed That Shall Bring Others, it
+    // triggers more items to be loaded.
+    divs.forEach(function(div : JQuery) {
+
+      var onViewChange = function(event, isInView : bool) {
+        var item : ClItem = div.data("self");
+
+        if (isInView) {
+          fillDiv(div);
+        } else {
+          emptyDiv(div);
+        }
+
+        if (isInView && item._id === feedIdThatLoadsMoreItems) {
+          // if this ajax call takes a while, get rid of
+          // feedIdThatLoadsMoreItems so that more re-scrolls don't load more
+          // and more items while you're waiting
+          feedIdThatLoadsMoreItems = "";
+          var allDivs : JQuery = $(".itemContainer");
+          Misc.assert(allDivs.length > 0);
+          var lastItem : ClItem = $(allDivs[allDivs.length-1]).data("self");
+          $.ajax({
+            type: 'get',
+            url: "/next-n-items",
+            data: {
+              feedId: lastItem.feedId,
+              date: lastItem.date,
+              n: NUM_ADDITIONAL_ITEMS,
+            },
+            success: function(data : string) {
+              var items : ClItem[] = JSON.parse(data);
+              insertItems(items);
+            },
+          });
+        }
+      };
+
+      div.bind('inview', onViewChange);
+
+      // $(window).one('scroll', function() {
+      //   console.log(div.data("inview"));
+      //   // onViewChange(null, div.data("inview"));
+      // });
+
+    });
   };
 
   export var enterRead = function(feed : ClFeed, callback ?: () => void) {
